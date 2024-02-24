788c7cb1741b62dae4e3f66bdb6595b79742de4f
LAMP-Lord <bgreninger@creativecag.com>
2024-02-24 14:17:37 -0600

    Fixed error in setReference

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -23,7 +23,7 @@ public class Feeder extends SubsystemBase {
 
 
     public Feeder(){
-        this.feederController = new SparkController(Constants.Setup.feedRoller, new SparkControllerInfo().feeder(Constants.Setup.feederInvert));
+        this.feederController = new SparkController(Constants.Setup.feedRoller, new SparkControllerInfo().feeder());
         
         this.feederEncoder = feederController.sparkEncode;
        
@@ -42,7 +42,7 @@ public class Feeder extends SubsystemBase {
             tangentialVelocity = Constants.Feeder.maxTanVel;
         }
         
-        feederController.setReference(tangentialVelocity, CANSparkBase.ControlType.kVelocity, 0, feedForward.calculate(tangentialVelocity/Constants.ConversionFactors.feederBaseConversionFactor));
+        feederPIDController.setReference(tangentialVelocity, CANSparkBase.ControlType.kVelocity, 0, feedForward.calculate(tangentialVelocity/Constants.ConversionFactors.feederBaseConversionFactor));
     }
 
     public void setDutyCylce(double percent){
