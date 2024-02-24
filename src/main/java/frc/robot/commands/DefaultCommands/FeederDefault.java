250412a801f34c9b833f82c2c528b90927795e2c
LAMP-Lord <bgreninger@creativecag.com>
2024-02-24 14:23:43 -0600

    Added FeederDefault

---
+++ src/main/java/frc/robot/commands/DefaultCommands/FeederDefault.java
@@ -0,0 +1,44 @@
+package frc.robot.commands.DefaultCommands;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Feeder;
+
+public class FeederDefault extends Command{
+    private Feeder s_Feeder;
+    private BooleanSupplier activeSup;
+    private DoubleSupplier speedSup;
+
+    public FeederDefault(
+        Feeder s_Feeder,
+        BooleanSupplier activeSup,
+        DoubleSupplier speedSup) {
+
+    this.s_Feeder = s_Feeder;
+    addRequirements(s_Feeder);
+    
+    this.activeSup = activeSup;
+    this.speedSup = speedSup;
+    }
+
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public void execute(){
+        if(activeSup.getAsBoolean() == true){
+            s_Feeder.setVelocity(speedSup.getAsDouble());
+        } else {
+            s_Feeder.setDutyCylce(0);
+        }
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+}
